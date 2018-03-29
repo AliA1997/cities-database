fb4ef4f2cd87dee4f6fcbb2b83ec49e60d137943
AliA1997 <alialhaddadAA@outlook.com>
2018-03-29 11:14:52 -0700

    Commit

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ const dotenv =require('dotenv')
 dotenv.config();
 */
 console.log(process.env.CONNECTION_STRING);
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+const app = express();
+app.use(bodyParser.json());
+
+//Connect to massive 
+massive(process.env.CONNECTION_STRING).then(database => {
+    //Sets the database.
+    app.set('db', database);
+    database.get_heroes().then(heroes => {
+        console.log(heroes);
+    })
+})
+app.get('/api/heroes', (req, res) => {
+    //Scoping issue
+    req.app.get('db').get_heroes().then(heroes => {
+        res.json({heroes: heroes})
+    })
+})
+
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log('Server listening on port: ' + PORT);
+})
